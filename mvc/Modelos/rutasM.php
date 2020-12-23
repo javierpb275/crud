0baefa6abd5536cbc4626dedcdbbdecdfd505ed8
javierpb275 <javierpb275@gmail.com>
2020-12-23 18:48:13 +0100

    creates class Modelo and inside the rutasModelo() function.
    this function receibes the parameter $rutas and this function will take us to some page depending on that parameter that we pass in.

--- mvc/Modelos/rutasM.php
+++ mvc/Modelos/rutasM.php
@@ -1,5 +1,24 @@
 <?php
 
+class Modelo {
 
+	static public function rutasModelo($rutas) {
+
+		if($rutas == "ingreso" || $rutas == "registrar" || $rutas == "empleados" || $rutas == "salir") {
+			$pagina = "Vistas/modulos".$rutas.".php";
+		}
+
+		else if($rutas == "index") {
+			$pagina = "Vistas/modulos/ingreso.php";
+		}
+
+		else {
+			$pagina = "Vistas/modulos/ingreso.php";
+		}
+
+		return $pagina;
+	}
+	
+}
 
 ?>
