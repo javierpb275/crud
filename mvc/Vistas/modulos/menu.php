652bbcc16ad4f28fd460b694bf237be316f883f0
javierpb275 <javierpb275@gmail.com>
2020-12-23 18:04:48 +0100

    removed nav from plantilla.php and moved nav to our new file menu.php in the modulos folder.

---
+++ mvc/Vistas/modulos/menu.php
@@ -0,0 +1,10 @@
+<nav>
+	<ul>
+		
+		<li><a href="index.html">Ingresar</a></li>
+		<li><a href="registrar.html">Registrar</a></li>
+		<li><a href="empleados.html">Empleados</a></li>
+		<li><a href="salir.html">Salir</a></li>
+
+	</ul>
+</nav>
--- mvc/Vistas/plantilla.php
+++ mvc/Vistas/plantilla.php
@@ -10,16 +10,7 @@
 
 <body>
 
-<nav>
-	<ul>
-		
-		<li><a href="index.html">Ingresar</a></li>
-		<li><a href="registrar.html">Registrar</a></li>
-		<li><a href="empleados.html">Empleados</a></li>
-		<li><a href="salir.html">Salir</a></li>
 
-	</ul>
-</nav>
 
 <section>
 
