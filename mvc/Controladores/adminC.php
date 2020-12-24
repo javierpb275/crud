2fc821f2ade3fa102c71e7a76425196add962cd6
javierpb275 <javierpb275@gmail.com>
2020-12-24 16:43:44 +0100

    added conditional in adminC in function IngresoC() so that it checks if the information entered by user is the right one.

--- mvc/Controladores/adminC.php
+++ mvc/Controladores/adminC.php
@@ -10,7 +10,20 @@ class AdminC {
 			$tablaBD = "administradores";
 
 			$respuesta = AdminM::IngresoM($datosC, $tablaBD);
-			
+
+			if ($respuesta["usuario"] == $_POST["usuarioI"] && $respuesta["clave"] == $_POST["claveI"]) {
+
+				session_start():
+
+				$_SESSION["Ingreso"] == true;
+
+				header("location:index.php?ruta=empleados");
+			}
+
+			else {
+				echo "ERROR AL INGRESAR";
+			}
+
 		}
 
 
