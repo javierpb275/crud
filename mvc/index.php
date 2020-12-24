fd2981a348c11768a02221e03a82cbc9b9a9ab9d
javierpb275 <javierpb275@gmail.com>
2020-12-24 14:29:02 +0100

    created adminC (controller) and adminM (model) and required in index.php

--- mvc/index.php
+++ mvc/index.php
@@ -1,7 +1,12 @@
 <?php
 
+//Controllers
 require_once "Controladores/rutasC.php";
+require_once "Controladores/adminC.php";
+
+//Models:
 require_once "Modelos/rutasM.php";
+require_once "Modelos/adminM.php";
 
 $rutas = new RutasControlador();
 $rutas -> plantilla();
