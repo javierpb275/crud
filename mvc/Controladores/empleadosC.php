9a18e44ef29d26b5ef86e2612184f2d13511496c
javierpb275 <javierpb275@gmail.com>
2020-12-25 12:34:29 +0100

    created EmpleadosC class and creating RegistrarEmpleadosC() function the same way that we did with adminC

---
+++ mvc/Controladores/empleadosC.php
@@ -0,0 +1,22 @@
+<?php 
+
+class EmpleadosC {
+
+	public function RegistrarEmpleadosC() {
+
+		if(isset($_POST["nombreR"])) {
+
+			$datosC =  array("nombre" => $_POST["nombreR"], "apellido" => $_POST["apellidoR"],
+							"email" => $_POST["emailR"], "puesto" => $_POST["puestoR"],
+							"salario" => $_POST["salarioR"]);
+
+			$tablaBD = "empleados";
+
+			$respuesta = EmpleadosM::RegistrarEmpleadosM($datosC, $tablaBD);
+
+			
+		}
+	}
+}
+
+?>
--- mvc/index.php
+++ mvc/index.php
@@ -3,10 +3,12 @@
 //Controllers
 require_once "Controladores/rutasC.php";
 require_once "Controladores/adminC.php";
+require_once "Controladores/empleadosC.php";
 
 //Models:
 require_once "Modelos/rutasM.php";
 require_once "Modelos/adminM.php";
+require_once "Modelos/empleadosM.php";
 
 $rutas = new RutasControlador();
 $rutas -> plantilla();
