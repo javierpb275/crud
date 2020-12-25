83b9e47581b2498ba0d08ab07de0883081587cd2
javierpb275 <javierpb275@gmail.com>
2020-12-25 13:19:04 +0100

    removed the tr from empleados.php.
    moved the tr to our MostrarEmpleadosC() function where we loop through all the employes and show their info.

--- mvc/Controladores/empleadosC.php
+++ mvc/Controladores/empleadosC.php
@@ -2,7 +2,7 @@
 
 class EmpleadosC {
 
-	//Registrar los empleados
+	//Registrar empleados
 	public function RegistrarEmpleadosC() {
 
 		if(isset($_POST["nombreR"])) {
@@ -34,6 +34,21 @@ class EmpleadosC {
 		$tablaBD = "empleados";
 
 		$respuesta = EmpleadosM::MostrarEmpleadosM($tablaBD);
+
+		foreach($respuesta as $key => $value) {
+
+			echo '
+			<tr>
+				<td>'.$value["nombre"].'</td>
+				<td>'.$value["apellido"].'</td>
+				<td>'.$value["email"].'</td>
+				<td>'.$value["puesto"].'</td>
+				<td>'.$value["salario"].'</td>
+				<td><button>Editar</button></td>
+				<td><button>Borrar</button></td>
+			</tr>';
+
+		}
 
 	}
 }
